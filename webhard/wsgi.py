f9b93760150df1b03324212cfb80a7a8f1f8cadd
liuzihao <68308963+liuzihaohao@users.noreply.github.com>
2022-02-26 19:01:11 +0800

    fist
    fist

---
+++ webhard/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for webhard project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webhard.settings')
+
+application = get_wsgi_application()
